1c43dfda2992daaa8f2207e7fde55a41c14fdaf2
Azeezat Raheem <azeezat@users.noreply.github.com>
2025-06-13 04:42:03 -0700

    Chore: Dependency updates (#110)

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
@@ -10,7 +10,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: [
           {
-            [pak.name]: path.join(__dirname, '..', pak.source),
+            [pak.name]: path.join(__dirname, '..', pak.source), //Add alias for package here
           },
         ],
       },
